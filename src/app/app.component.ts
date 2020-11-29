67e05dbe1ba57bc3404dfeaedcd3bbf1be0d0ca6
Sanjana Jain <sanjana.jain04@gmail.com>
2020-11-29 15:53:27 +0530

    Adding the App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+//https://material.angular.io/
+//ng g c component_name
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
+  imgUrl ="../assets/CG_logo.png";
+  url = "https://www.capgemini.com";
+  userName: string = "";
+  nameError: string = "";
+  
+  ngOnInit(): void{
+    this.title = "Hello from Capgemini.";
+  }
+
+  onClick($event){
+    console.log("Savebutton is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event){
+    console.log("Change Event Occured!",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { Module } from 'module';
+import { DemoComponent } from './component/demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
